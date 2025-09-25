c64d9f88bb885108b7288d59c0fbf9345f9bd8e6
Vitor Gonzaga <vitorres100@outlook.com>
2025-09-25 09:54:36 -0300

    fix: deploy issues

---
+++ global.d.ts
@@ -0,0 +1,16 @@
+declare function push(): void;
+declare function pop(): void;
+declare function stroke(color: any): void;
+declare function strokeWeight(weight: number): void;
+declare function strokeJoin(style: any): void;
+declare function fill(color: any): void;
+declare function beginShape(): void;
+declare function vertex(x: number, y: number): void;
+declare function endShape(mode?: any): void;
+declare function noFill(): void;
+declare function noStroke(): void;
+declare function ellipse(x: number, y: number, w: number, h?: number): void;
+declare function cos(angle: number): number;
+declare function sin(angle: number): number;
+declare function degrees(radians: number): number;
+declare function radians(degrees: number): number;
